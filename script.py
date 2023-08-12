c28963897fdbbbf01084d2a45e78b28c2d9f480c
NarisR <nariscatboy@gmail.com>
2023-08-12 10:35:05 +1000

    lint

--- script.py
+++ script.py
@@ -1,15 +1,15 @@
-## reqiures 3.8+
+# reqiures 3.8+
 import asyncio
-from datetime import datetime, timedelta
+from datetime import datetime
 import re
 import httpx
-import json
 from getpass import getpass
 import subprocess as sp
 from configparser import ConfigParser
 import logging
 import sys
 from typing import Dict, List
+from contextlib import suppress
 
 import audible
 from bs4 import BeautifulSoup
@@ -19,133 +19,140 @@ logger = logging.getLogger(__name__)
 
 
 def captcha(url):
-	'''
-	param: url - captcha url
-	returns: captcha answer
-	'''
-	sp.run(['python', '-m', 'webbrowser', url])
-	return input(f'CAPTCHA {url} :')
+    '''
+    param: url - captcha url
+    returns: captcha answer
+    '''
+    sp.run(['python', '-m', 'webbrowser', url])
+    return input(f'CAPTCHA {url} :')
 
 
 def login():
-	'''
-	asks user for login credentials
-	returns: audible client (logged in)
-	'''
-	logger.info('logging in')
-	auth_file = '.audible_auth'
-	try:
-		auth = audible.Authenticator.from_file(auth_file)
-		client = audible.Client(auth=auth)
-		client.get('library', num_results=1)
-	except (FileNotFoundError, audible.exceptions.AuthFlowError) as e:
-		user = input('Username (email): ')
-		passwd = getpass()
-		auth = audible.Authenticator.from_login(
-			user,
-			passwd,
-			locale="AU",
-			with_username=False,
-			captcha_callback=captcha,
-		)
-		auth.register_device()
-		auth.to_file(auth_file)
-		client = audible.Client(auth=auth)
-	return client
+    '''
+    asks user for login credentials
+    returns: audible client (logged in)
+    '''
+    logger.info('logging in')
+    auth_file = '.audible_auth'
+    try:
+        auth = audible.Authenticator.from_file(auth_file)
+        client = audible.Client(auth=auth)
+        client.get('library', num_results=1)
+    except (FileNotFoundError, audible.exceptions.AuthFlowError):
+        user = input('Username (email): ')
+        passwd = getpass()
+        auth = audible.Authenticator.from_login(
+            user,
+            passwd,
+            locale="AU",
+            with_username=False,
+            captcha_callback=captcha,
+        )
+        auth.register_device()
+        auth.to_file(auth_file)
+        client = audible.Client(auth=auth)
+    return client
 
 # ================================== core =====================================
-	
+
+
 def get_owned_series(client):
-	'''
-	param: client - audbile client
-	returns: mapping from title of series in library to series info
-		including the latest owned book
-	'''
-	logger.info('retrieving library')
-	library = client.get(
-		'library',
-		num_results=1000,
-		response_groups=','.join([
-			'series',
-			'product_desc',
-			'product_attrs',
-		]),
-		sort_by='-PurchaseDate',
-	)
-	has_series = [book for book in library['items'] if book.get('series')]
-	owned = {}
-	for book in has_series:
-		series = book['series'][0]
-		book = {
-			'asin': book['asin'],
-			'title': book['title'],
-			'subtitle': book.get('subtitle'),
-			'release_date': datetime.strptime(
-				book['release_date'],
-				'%Y-%m-%d'
-			)
-		}
-		series['latest'] = book
-		series = owned.setdefault(series['title'], series)
-		if series['latest']['release_date'] < book['release_date']:
-			series['latest'] = book
-	return owned
+    '''
+    param: client - audbile client
+    returns: mapping from title of series in library to series info
+            including the latest owned book
+    '''
+    logger.info('retrieving library')
+    library = client.get(
+        'library',
+        num_results=1000,
+        response_groups=','.join([
+            'series',
+            'product_desc',
+            'product_attrs',
+        ]),
+        sort_by='-PurchaseDate',
+    )
+    has_series = [book for book in library['items'] if book.get('series')]
+    owned = {}
+    for book in has_series:
+        series = book['series'][0]
+        book = {
+            'asin': book['asin'],
+            'title': book['title'],
+            'subtitle': book.get('subtitle'),
+            'release_date': datetime.strptime(
+                book['release_date'],
+                '%Y-%m-%d'
+            )
+        }
+        series['latest'] = book
+        series = owned.setdefault(series['title'], series)
+        if series['latest']['release_date'] < book['release_date']:
+            series['latest'] = book
+    return owned
+
 
 def format_release(release):
-	'''
-	param: release date (datetime)
-	returns: if book is released: empty string
-			 or time till release rounded down by days
-			 or not rounded if less than 1 day
-	'''
-	release = release.replace(hour=17)
-	today = datetime.today().replace(microsecond=0)
-	if release <= today: return ''
-	diff = release - today
-	if diff.days > 0: return f': in {diff.days} days'
-	else: return f': in {diff}'
+    '''
+    param: release date (datetime)
+    returns: if book is released: empty string
+                     or time till release rounded down by days
+                     or not rounded if less than 1 day
+    '''
+    release = release.replace(hour=17)
+    today = datetime.today().replace(microsecond=0)
+    if release <= today:
+        return ''
+    diff = release - today
+    if diff.days > 0:
+        return f': in {diff.days} days'
+    else:
+        return f': in {diff}'
+
 
 async def check_releases(http_client, series):
-	'''
-	params:
-		http_client: httpx async client
-		series: a list of book series from audble client
-			containing url, title, and the custom property 'latest'
-	returns: a list of unowned books older than the 'latest' entries in the series
-	'''
-	url = series['url']\
-		.replace('/pd/', 'https://www.audible.com.au/series/')\
-		.replace('Audiobook/', 'Audiobooks/')
-	response = await http_client.get(url, timeout=30, )
-	logger.info(f"checking {series['title']} {response.status_code}")
-	page = BeautifulSoup(response.content, 'html.parser')
-	releases = page.select('.releaseDateLabel')
-	today = datetime.today()
-	return series['title'], {
-		node.find_parent('ul')
-			.select('.bc-heading a.bc-link')[0]
-			.get_text():
-			release_date
-		for node in releases
-		if (release_date := datetime.strptime(
-			re.search(
-				r'\d+-\d+-\d+',
-				node.get_text()
-			).group(0),
-			'%d-%m-%Y',
-		)) > series['latest']['release_date']
-	}
+    '''
+    params:
+            http_client: httpx async client
+            series: a list of book series from audble client
+                    containing url, title, and the custom property 'latest'
+    returns: a list of unowned books older than 'latest' in series
+    '''
+    url = series['url']\
+        .replace('/pd/', 'https://www.audible.com.au/series/')\
+        .replace('Audiobook/', 'Audiobooks/')
+    response = await http_client.get(url, timeout=30, )
+    logger.info(f"checking {series['title']} {response.status_code}")
+    page = BeautifulSoup(response.content, 'html.parser')
+    releases = page.select('.releaseDateLabel')
+    return series['title'], {
+        node.find_parent('ul')
+        .select('.bc-heading a.bc-link')[0]
+        .get_text():
+        release_date
+        for node in releases
+        if (release_date := datetime.strptime(
+            re.search(
+                r'\d+-\d+-\d+',
+                node.get_text()
+            ).group(0),
+            '%d-%m-%Y',
+        )) > series['latest']['release_date']
+    }
+
 
 def display(releases: Dict[str, List[str]]):
-	sorted_releases = sorted(
-		[(s, r) for s, r in releases.items() if r],
-		key=lambda sr: min(sr[1].values())
-	)
-	for series, books in sorted_releases:
-		print(f'# {series}')
-		for book, release_date in books.items():
-			print(f'- {book}' + format_release(release_date))
-		print()
+    sorted_releases = sorted(
+        [(s, r) for s, r in releases.items() if r],
+        key=lambda sr: min(sr[1].values())
+    )
+    for series, books in sorted_releases:
+        print(f'# {series}')
+        for book, release_date in books.items():
+            print(f'- {book}' + format_release(release_date))
+        print()
+
 
 # ======================== config =================================
 '''
@@ -156,38 +163,38 @@ def display(releases: Dict[str, List[str]]):
 ...
 '''
 
+
 def get_config(filename='config.ini'):
-	config = ConfigParser()
-	try:
-		config.read('config.ini')
-		return {
-			'ignore_series': [
-				item for key, item in config.items('ignore_series')
-			],
-		}
-	except:
-		return {}
+    config = ConfigParser()
+    with suppress():
+        config.read('config.ini')
+        return {
+            'ignore_series': [
+                item for key, item in config.items('ignore_series')
+            ],
+        }
+    return {}
 
 # ============================= main ==================================
 
+
 async def main():
-	config = get_config()
+    config = get_config()
 
-	client = login()
-	owned = {
-		title:owned
-		for title,owned in get_owned_series(client).items()
-		if title not in config.get('ignore_series', [])
-	}
-	async with httpx.AsyncClient() as http_client:
-		new_releases = await tqdm.gather(*(
-			check_releases(http_client, series)
-			for series in owned.values()
-		))
-	display(dict(new_releases))
-
+    client = login()
+    owned = {
+        title: owned
+        for title, owned in get_owned_series(client).items()
+        if title not in config.get('ignore_series', [])
+    }
+    async with httpx.AsyncClient() as http_client:
+        new_releases = await tqdm.gather(*(
+            check_releases(http_client, series)
+            for series in owned.values()
+        ))
+    display(dict(new_releases))
 
 
 if __name__ == '__main__':
-	logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
-	asyncio.run(main())
+    logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
+    asyncio.run(main())
